dd742a45166b26001b59f745e20fc64df5bf358e
zimoyin <2556608754@qq.com>
2025-02-09 17:47:19 +0800

    feat(utils): 添加截屏功能并优化 ChromeDriver 使用
    - 新增 screenshot() 扩展函数，支持截取不同类型的屏幕截图
    - 修改 use() 函数，使其仅适用于 ChromeDriver 类型
    -引入必要的包以支持截屏功能

--- src/main/kotlin/io/github/headlesschrome/utils/Ex.kt
+++ src/main/kotlin/io/github/headlesschrome/utils/Ex.kt
@@ -1,17 +1,50 @@
 package io.github.headlesschrome.utils
 
-import org.openqa.selenium.WebDriver
+import org.openqa.selenium.OutputType
+import org.openqa.selenium.TakesScreenshot
 import org.openqa.selenium.chrome.ChromeDriver
+import java.awt.image.BufferedImage
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.InputStream
+import javax.imageio.ImageIO
+
 
 /**
  *
  * @author : zimo
  * @date : 2025/02/09
  */
-inline fun ChromeDriver.use(block: WebDriver.() -> Unit) {
+inline fun ChromeDriver.use(block: ChromeDriver.() -> Unit) {
     try {
         block()
     } finally {
         quit()
+    }
+}
+
+
+inline fun <reified T:Any> ChromeDriver.screenshot(): T {
+    return when(T::class.java){
+        BufferedImage::class.java -> {
+            ImageIO.read(getScreenshotAs<File>(OutputType.FILE)) as T
+        }
+        String::class.java -> {
+            getScreenshotAs<String>(OutputType.BASE64) as T
+        }
+        File::class.java -> {
+            getScreenshotAs<File>(OutputType.FILE) as T
+        }
+        ByteArray::class.java -> {
+            getScreenshotAs<ByteArray>(OutputType.BYTES) as T
+        }
+        InputStream::class.java -> {
+            ByteArrayInputStream(getScreenshotAs<ByteArray>(OutputType.BYTES)) as T
+        }
+        Any::class.java -> {
+            getScreenshotAs<String>(OutputType.BASE64) as T
+        }
+        else -> throw IllegalArgumentException("Unsupported type: ${T::class.java}")
     }
 }
